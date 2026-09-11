0cfb2cb6cd3a4e1c630a0c85736e400ce5997edd
juliocesar94ls21 <juliocesar94ls21@gmail.com>
2026-09-11 16:21:34 -0300

    primeira publicacao

---
+++ js/global.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+
+  function animateOnScroll(element, animationClass) {
+        // Deixa os elementos invisíveis no início
+        $(element).addClass('animate-on-scroll');
+
+        $(window).on('scroll load', function() {
+            $(element).each(function() {
+                var elementTop = $(this).offset().top;
+                var windowBottom = $(window).scrollTop() + $(window).height();
+
+                // Quando o elemento entra na tela
+                if(windowBottom > elementTop + 50) { // 50px de folga
+                    if(!$(this).hasClass('animate__animated')) {
+                        $(this).css('opacity', 1); // torna visível
+                        $(this).addClass('animate__animated ' + animationClass);
+                    }
+                }
+            });
+        });
+    }
+ /* 
+  animateOnScroll('.title-header-bg', 'animate__fadeInUp');
+  animateOnScroll('.paragraf-header-bg', 'animate__fadeInUp');
+  animateOnScroll('.btn-primary-bc', 'animate__fadeInUp');
+  animateOnScroll('.social-icons-bc', 'animate__fadeInUp');
+  animateOnScroll('.entry-text-qs-paragraf', 'animate__fadeInUp');
+  animateOnScroll('.entry-text-number-qs', 'animate__fadeInUp');
+  animateOnScroll('.entry-text-qs', 'animate__fadeInUp');
+  animateOnScroll('.img-qs', 'animate__fadeInUp');
+  animateOnScroll('.img-services-bc', 'animate__fadeInUp');
+  animateOnScroll('.icon-number-services-bc', 'animate__fadeInUp');
+  animateOnScroll('.title-services-item', 'animate__fadeInUp');
+  animateOnScroll('.title-serv-price', 'animate__fadeInUp');
+  animateOnScroll('.price1', 'animate__fadeInUp');
+  animateOnScroll('.prince2', 'animate__fadeInUp');
+  animateOnScroll('.theme-color-secondary-text', 'animate__fadeInUp');
+  animateOnScroll('.col-price', 'animate__fadeInUp');
+  animateOnScroll('.col-test-desc', 'animate__fadeInUp');
+  animateOnScroll('.paagraf-test', 'animate__fadeInUp');
+  animateOnScroll('.profs-prof', 'animate__fadeInUp');
+  animateOnScroll('.img-perfil-test', 'animate__fadeInUp');
+  animateOnScroll('.price3', 'animate__fadeInUp');*/
+animateOnScroll('.title-header-bg', 'animate__fadeInUp');
+animateOnScroll('.paragraf-header-bg', 'animate__fadeInUp');
+animateOnScroll('.btn-primary-bf', 'animate__fadeInUp');
+animateOnScroll('.col-card-bf', 'animate__fadeInUp');
+animateOnScroll('.title-card-bf', 'animate__fadeInUp');
+animateOnScroll('.paragraf-card-bf', 'animate__fadeInUp');
+animateOnScroll('.col-img-el', 'animate__fadeInLeft');
+animateOnScroll('.title-content-qs', 'animate__fadeInUp');
+animateOnScroll('.desc-content-qs', 'animate__fadeInUp');
+animateOnScroll('.col-data', 'animate__fadeInUp');
+animateOnScroll('.unwrap-desc-qs', 'animate__fadeInUp');
+animateOnScroll('.col-foto-elaine', 'animate__fadeInUp');
+animateOnScroll('.inner-dp-text', 'animate__fadeInUp');
+animateOnScroll('.inner-dp-desc', 'animate__fadeInUp');
+animateOnScroll('.col-test-item', 'animate__fadeInUp');
+animateOnScroll('.col-iframe-left', 'animate__fadeInLeft');
+animateOnScroll('.col-iframe-right', 'animate__fadeInRight');
+animateOnScroll('.col-poster-bc', 'animate__fadeInRight');
+animateOnScroll('.text-input-bc', 'animate__fadeInUp');
+
+$(".fa-icon-menu").on("click", function(){
+    $(".nav-header").toggle("slow");
+});
+
+})
+  	
+function isMobile(){
+    return $(window).outerWidth() < 768;
+}
+
+$(window).on('load', function () {
+    $(".overlaypt").remove();
+  });
